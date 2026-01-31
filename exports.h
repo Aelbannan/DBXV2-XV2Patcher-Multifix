87cc677f1b174c62bde820e1163c9a845d191b09
Ahmed <akelbann@gmail.com>
2026-01-31 08:17:03 +0800

    Fix crash

--- exports.h
+++ exports.h
@@ -1,11 +1,17 @@
-﻿//  exports.h
-//
-//	Simple header to instruct the linker to forward function exports to another library.
-//
-
-#pragma comment(linker,"/export:DirectInput8Create=DINPUT8_orig.DirectInput8Create,@1")
-#pragma comment(linker,"/export:DllCanUnloadNow=DINPUT8_orig.DllCanUnloadNow,@2")
-#pragma comment(linker,"/export:DllGetClassObject=DINPUT8_orig.DllGetClassObject,@3")
-#pragma comment(linker,"/export:DllRegisterServer=DINPUT8_orig.DllRegisterServer,@4")
-#pragma comment(linker,"/export:DllUnregisterServer=DINPUT8_orig.DllUnregisterServer,@5")
-#pragma comment(linker,"/export:GetdfDIJoystick=DINPUT8_orig.GetdfDIJoystick,@6")
+﻿#pragma comment(linker, "/export:GetFileVersionInfoA=C:\\Windows\\System32\\version.GetFileVersionInfoA")
+#pragma comment(linker, "/export:GetFileVersionInfoByHandle=C:\\Windows\\System32\\version.GetFileVersionInfoByHandle")
+#pragma comment(linker, "/export:GetFileVersionInfoExA=C:\\Windows\\System32\\version.GetFileVersionInfoExA")
+#pragma comment(linker, "/export:GetFileVersionInfoExW=C:\\Windows\\System32\\version.GetFileVersionInfoExW")
+#pragma comment(linker, "/export:GetFileVersionInfoSizeA=C:\\Windows\\System32\\version.GetFileVersionInfoSizeA")
+#pragma comment(linker, "/export:GetFileVersionInfoSizeExA=C:\\Windows\\System32\\version.GetFileVersionInfoSizeExA")
+#pragma comment(linker, "/export:GetFileVersionInfoSizeExW=C:\\Windows\\System32\\version.GetFileVersionInfoSizeExW")
+#pragma comment(linker, "/export:GetFileVersionInfoSizeW=C:\\Windows\\System32\\version.GetFileVersionInfoSizeW")
+#pragma comment(linker, "/export:GetFileVersionInfoW=C:\\Windows\\System32\\version.GetFileVersionInfoW")
+#pragma comment(linker, "/export:VerFindFileA=C:\\Windows\\System32\\version.VerFindFileA")
+#pragma comment(linker, "/export:VerFindFileW=C:\\Windows\\System32\\version.VerFindFileW")
+#pragma comment(linker, "/export:VerInstallFileA=C:\\Windows\\System32\\version.VerInstallFileA")
+#pragma comment(linker, "/export:VerInstallFileW=C:\\Windows\\System32\\version.VerInstallFileW")
+#pragma comment(linker, "/export:VerLanguageNameA=C:\\Windows\\System32\\version.VerLanguageNameA")
+#pragma comment(linker, "/export:VerLanguageNameW=C:\\Windows\\System32\\version.VerLanguageNameW")
+#pragma comment(linker, "/export:VerQueryValueA=C:\\Windows\\System32\\version.VerQueryValueA")
+#pragma comment(linker, "/export:VerQueryValueW=C:\\Windows\\System32\\version.VerQueryValueW")
